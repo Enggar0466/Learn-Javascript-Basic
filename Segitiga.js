74b83f46b9d5ae4e4c544d5054be23cfd70a7d6c
Enggar0466 <115142147+Enggar0466@users.noreply.github.com>
2022-11-16 13:12:02 +0700

    Create Segitiga.js
    add Function menghitungLuas & menghitungLuasSegitiga

---
+++ Segitiga.js
@@ -0,0 +1,19 @@
+
+function menghitungLuas(p, l) {
+    //solve logic
+        const result= p * l;
+    //result 
+        console.log('result = ', result , 'cm')
+    }
+    //calling function
+    menghitungLuas (1,2)
+
+function luasSegitiga (a, t) {
+    //solve logic
+        const result = 0.5 * a * t;
+    //result
+        console.log ('result = ', result ,'cm2')
+
+}
+    //calling function
+    luasSegitiga (2,4)
