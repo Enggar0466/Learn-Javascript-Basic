516f6757960eada73701396d2dbb351932395060
Enggar0466 <115142147+Enggar0466@users.noreply.github.com>
2022-11-16 14:12:36 +0700

    Update and rename Segitiga.js to keliling.js
    revisi

---
+++ keliling.js
@@ -0,0 +1,27 @@
+function luasLingkaran(r1, r2) {
+    //solve logic
+        const result= 3.14 * r1 * r2;
+    //result 
+        console.log('result = ', result , 'cm2')
+    }
+    //calling function
+    luasLingkaran (1,2)
+
+function luasSegitiga (a, t) {
+    //solve logic
+        const result = 0.5 * a * t;
+    //result
+        console.log ('result = ', result ,'cm2')
+
+}
+    //calling function
+    luasSegitiga (2,4)
+
+    function luasPersegi(s1, s2, s3, s4 ) {
+        //solve logic
+            const result= s1+s2+s3+s4;
+        //result 
+            console.log('result = ', result , 'cm')
+        }
+        //calling function
+        luasPersegi (5,4,3,2)
